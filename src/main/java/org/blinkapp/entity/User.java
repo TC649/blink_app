173494f11f91bbb99650816eec790042c36e0e4b
Tom  Campbell <tomcampbell@Toms-Laptop.local>
2026-08-17 21:51:59 +1000

    Permissions and migrations implementation

--- src/main/java/org/blinkapp/entity/User.java
+++ src/main/java/org/blinkapp/entity/User.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -22,8 +23,8 @@ public class User {
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "user_roles",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "role_id"))
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
 }
